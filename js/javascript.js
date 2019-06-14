8a87071a3293f3c83548015dd82ff5f444fa6d17
Nikita Levitski <reflevius@gmail.com>
2019-06-15 00:39:32 +0300

    owl-carousel, fontawesome, almost ready mobile

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,31 @@
 /* НАЧАЛО АДАПТИВНАЯ КАРУСЕЛЬ нa owv-carousel */
 $(document).ready(function() {
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        responsive:{
+            0:{
+                items:1
+            },
+            1440:{
+                items:1
+            }
+        }
+
+
+    })
+    var owl = $('.owl-carousel');
+    owl.owlCarousel();
+    // Go to the next item
+    $('.nextslidepos').click(function() {
+        owl.trigger('next.owl.carousel');
+    })
+    // Go to the previous item
+    $('.prevslidepos').click(function() {
+        // With optional speed parameter
+        // Parameters has to be in square bracket '[]'
+        owl.trigger('prev.owl.carousel', [300]);
+    })
+
 
 });
